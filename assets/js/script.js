dddb5d33bcdb08433e2356e1f81da902f38fc21b
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-08-07 20:46:57 +0000

    renaming link0 to ireland edge and changing hyperlinks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
+const menuButton = document.getElementById("menu-btn")
+if(menuButton != null){
+    menuButton.addEventListener('click', lastLoad);
+}
+
 function openInstagram(){
     window.open('https://www.instagram.com/anthonya_d/');
-};
+}
 
-
+function lastLoad(){
+    window.history.go(-1); return false;
+}
